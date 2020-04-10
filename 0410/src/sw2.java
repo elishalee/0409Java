34c739a48b6d6ad2d274d259c4d45b970e56bdc9
elishalee <elishalee00@gmail.com>
2020-04-10 18:05:44 +0900

    수

--- 0410/src/sw2.java
+++ 0410/src/sw2.java
@@ -3,7 +3,7 @@ public class sw2 {
 
 	public static void main(String[] args) {
 		final String [] RED = {"빨강","빨간색"};
-		final String [] GREEN = {"초록,"초록색"};
+		final String [] GREEN = {"초록","초록색"};
 		
 		java.util.Scanner co = new java.util.Scanner(System.in);
 		System.out.println("잘 익은 사과의 색상을 말하시오");
@@ -13,7 +13,10 @@ public class sw2 {
 		   System.out.println("정답");
 	   }else if (color.contentEquals(GREEN[0]) || color.contentEquals(GREEN[1])){
 		   System.out.println("초록색은 아오리 사과입니다");
+	   }else {
+		   System.out.println("사과의 색상이 아닙니다");
 	   }
+	   
 	  co.close();
 	  
 	}
